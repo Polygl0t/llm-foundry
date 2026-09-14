b061497938fd63d111fab963825ff8d2c7a9e339
shende-bender <s6shtaoo@uni-bonn.de>
2026-09-14 11:35:02 +0200

    move definition of preprocess to fix data loading for non-main process

--- data/filters/train_annotator.py
+++ data/filters/train_annotator.py
@@ -223,14 +223,13 @@ def main(args):
             f"Text column '{args.text_column}' not found in the dataset. Available columns: {dataset['train'].column_names}"
         )
 
+    def preprocess(examples):
+        batch = tokenizer(examples[args.text_column], truncation=True)
+        batch["labels"] = np.float32(examples[args.target_column])
+        return batch
+
     # Only main process runs the preprocessing and mapping
     if master_process:
-
-        def preprocess(examples):
-            batch = tokenizer(examples[args.text_column], truncation=True)
-            batch["labels"] = np.float32(examples[args.target_column])
-            return batch
-
         dataset = dataset.map(
             preprocess,
             batched=True,
